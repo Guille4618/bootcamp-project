ea82b164f36b98598345367914d67bb32e7b4c74
Guille4618 <llebresguillermo@gmail.com>
2026-04-13 10:46:37 +0200

    fix: actualizar URL de la API al backend en Vercel

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
-if (!process.env.PORT) {
-    throw new Error('El puerto no está definido. Asegúrate de tener un archivo .env con PORT=3000');
-}
+
 module.exports = {
-    PORT: process.env.PORT,
-}
+    PORT: process.env.PORT || 3000
+};
