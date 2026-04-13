246f362fb4f686d66d8342c30a0d35eb62b8d710
Guille4618 <llebresguillermo@gmail.com>
2026-04-13 11:05:13 +0200

    fix: actualizar URL correcta del backend en Vercel

--- api/client.js
+++ api/client.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://bootcamp-project-ulf4.vercel.app/api/tasks';
+const API_URL = 'https://bootcamp-project-547b.vercel.app/api/tasks';
 
 async function obtenerTareas() {
     const response = await fetch(API_URL);
